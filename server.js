da26e1d5d5ce2370ae7bebffaeaa63af15064ede
sahir <sahir.vellani@gmail.com>
2016-01-21 23:18:15 -0500

    Removed other stuff

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 
-// Serve assets in /public.
 app.use(express.static(__dirname));
 
-// So we can POST.
-app.use(bodyParser.urlencoded());
-
-// The editor interface.
 app.get('/', function(req, res) {
   res.sendFile('index.html');
 });
