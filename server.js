171026665b458888c6eb8b5cc203896f1ee6fd99
sahir <sahir.vellani@gmail.com>
2016-01-21 23:17:13 -0500

    Accidentally added tutorial file. Fixed

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var cors = require('cors');
 
 // Serve assets in /public.
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 
 // So we can POST.
 app.use(bodyParser.urlencoded());
 
-// Since Mixmax calls this API directly from the client-side, it must be whitelisted.
-var corsOptions = {
-  origin: /^[^.\s]+\.mixmax\.com$/,
-  credentials: true
-};
-
 // The editor interface.
-app.get('/editor', function(req, res) {
-  res.sendFile(__dirname + '/editor.html');
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
 });
 
-// The in-email representation.
-app.post('/api/resolver', cors(corsOptions), require('./api/resolver'));
-
-app.listen(process.env.PORT || 8910);
+app.listen(3000);
+console.log('App running at 3000')
