30d7ca2f786af4a6f63a86e94be517029a8463a5
sahir <sahir.vellani@gmail.com>
2016-01-22 01:58:10 -0500

    functional but server needs https implemented

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 $(document).ready(function() {
-    var items;
+    var items = [];
     $('#search-go').on('click', function(){
         var searchItem = $('#item').val();
         $.ajax({
             url: 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + searchItem + '&api-key=bf143397b45979f05b3d4ad94d16e2ec:10:74093509',
             method: 'GET',
             success: function(res){
-                var articles = items = res.response.docs;
+                var articles = res.response.docs;
                 for (var i in articles){
                     var article = articles[i];
                     var headline = article.headline.main;
                     var snippet = article.snippet;
                     var articleURL = article.web_url;
                     
-                    $('#response').html($('#response').html() + '<div class="row section" data-item=\"' + i + '\"> <h3>' + headline + '</h3>'+'<p>' + snippet + '</p>' + '<h6> Click: <a href=' + articleURL + '>Here</a> for details.</h3></div>');
+                    var markup = '<div class="row section" data-item=\"' + i + '\"> <h3>' + headline + '</h3>'+'<p>' + snippet + '</p>' + '<h6> Click: <a href=' + articleURL + '>Here</a> for details.</h3></div>';
+                    console.log(markup);
+                    items.push(markup);
+                    $('#response').html($('#response').html() + markup);
                 }
                 console.log(res);
                 $('.section').on('click', function(){
-                    var selectedItem = items[$(this).attr('data-item')];
-                    console.log(selectedItem);
+                    var target = $(this);
+                    console.log(target.attr('data-item'));
+                    console.log(items[target.attr('data-item')].web_url);
+
+                    // Let Mixmax know it was done.
+                    Mixmax.done({
+                        src: items[target.attr('data-item')],
+                        raw: true
+                    });
                 });
             },
             error: function(res){
                 console.log(res);
             }                              
+        });
+        $.ajax({
+            
         });
     });
     
--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 var express = require('express');
+var https = require('https');
+var http = require('http');
 var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
 app.use(express.static(__dirname));
+
+// So we can POST.
+app.use(bodyParser.urlencoded());
 
 app.get('/', function(req, res) {
   res.sendFile('index.html');
 });
 
+// Since Mixmax calls this API directly from the client-side, it must be whitelisted.
+var corsOptions = {
+  origin: /^[^.\s]+\.mixmax\.com$/,
+  credentials: true
+};
+
+app.post('/add', cors(corsOptions), function(req, res){
+    console.log('req.body', req.body)
+    var data = JSON.parse(req.body.params);
+    if (!data) {
+        res.status(403 /* Unauthorized */ ).send('Invalid params');
+        return;
+    }
+
+    var html = data.src;
+
+    res.json({body: html});
+});
+
 app.listen(3000);
-console.log('App running at 3000')
+console.log('App running at 3000');
